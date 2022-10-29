ee3815110980eb0b38c0e82ca3ddfe5f3de553f6
dafinoer <dafinoer@outlook.com>
2022-10-29 16:21:40 +0700

    add test for iinsertion function

--- test/usecase/sorting_test.dart
+++ test/usecase/sorting_test.dart
@@ -1,6 +1,7 @@
 import 'package:algovisualizer/data/repository/reactive_repository.dart';
 import 'package:algovisualizer/domain/entity/visualizer.dart';
 import 'package:algovisualizer/domain/usecase/bubble_sort_usecase.dart';
+import 'package:algovisualizer/domain/usecase/insert_sort_usecase.dart';
 import 'package:algovisualizer/domain/usecase/selection_sort_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -15,14 +16,16 @@ void main() {
     () {
       late BubbleSortUseCase bubbleSortUseCase;
       late SelectionSortUsecase selectionSortUsecase;
+      late InsertSortUsecase insertSortUsecase;
       late MockReactiveRepository mockReactiveRepository;
       final unsortList = [1, 20, 3];
       setUp(() {
         mockReactiveRepository = MockReactiveRepository();
         bubbleSortUseCase = BubbleSortUseCase(mockReactiveRepository);
         selectionSortUsecase = SelectionSortUsecase(mockReactiveRepository);
+        insertSortUsecase = InsertSortUsecase(mockReactiveRepository);
       });
-      test('Bubble Sort test output function', () {
+      test('Bubble Sort output test', () {
         final result = bubbleSortUseCase.sorting(unsortList);
         expect(result, [1, 3, 20]);
       });
@@ -30,6 +33,11 @@ void main() {
       test('Selection Sort output test', () {
         final result = selectionSortUsecase.sorting(unsortList);
         expect(result, [1, 3, 20]);
+      });
+
+      test('Insertion Sort output test', () {
+        final result = insertSortUsecase.sorting([30, 0, 20]);
+        expect(result, [0, 20, 30]);
       });
     },
   );
