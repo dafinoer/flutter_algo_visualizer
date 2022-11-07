8a1751c3997cacb6ba414ea50d8eb4bacbdc0967
dafinoer <dafinoer@outlook.com>
2022-11-07 14:23:15 +0700

    abstrac class divide conquer

--- lib/domain/usecase/sorting_usecase.dart
+++ lib/domain/usecase/sorting_usecase.dart
@@ -1,3 +1,9 @@
 abstract class SortingUseCase {
   List<int> sorting(List<int> items);
 }
+
+abstract class DivideAndConquer implements SortingUseCase {
+  void setDefaultItems(List<int> items);
+
+  List<int> items();
+}
