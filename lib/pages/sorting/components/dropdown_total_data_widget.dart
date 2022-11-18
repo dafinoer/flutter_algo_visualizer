85ac44215637d5e389f9420c9783f8ff8e3a422f
dafinoer <dafinoer@outlook.com>
2022-11-18 08:30:21 +0700

    revamp ui

--- lib/pages/sorting/components/dropdown_total_data_widget.dart
+++ lib/pages/sorting/components/dropdown_total_data_widget.dart
@@ -9,26 +9,62 @@ class DropdownTotalDataWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sortingForm = Provider.of<SortingForm>(context, listen: false);
-    return Observer(
-      builder: (context) {
-        return DropdownButton(
-          value: sortingForm.totalItem,
-          hint: const Text('How many data'),
-          items: const [
-            DropdownMenuItem(value: 10, child: Text('10')),
-            DropdownMenuItem(value: 20, child: Text('20')),
-            DropdownMenuItem(value: 30, child: Text('30')),
-            DropdownMenuItem(value: 40, child: Text('40')),
-            DropdownMenuItem(value: 50, child: Text('50')),
-          ],
-          onChanged: sortingForm.isDisableActionForm
-              ? null
-              : (int? value) {
-                  final itemValue = value;
-                  if (itemValue != null) sortingForm.setTotalItem(itemValue);
-                },
-        );
-      },
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          border: Border.all(width: 0.5, color: Colors.grey)),
+      child: Observer(
+        builder: (context) {
+          return DropdownButton(
+            borderRadius: BorderRadius.circular(12.0),
+            value: sortingForm.totalItem,
+            isExpanded: true,
+            hint: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text('How many data'),
+            ),
+            underline: const SizedBox.shrink(),
+            items: const [
+              DropdownMenuItem(
+                  value: 10,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('10'),
+                  )),
+              DropdownMenuItem(
+                  value: 20,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('20'),
+                  )),
+              DropdownMenuItem(
+                  value: 30,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('30'),
+                  )),
+              DropdownMenuItem(
+                  value: 40,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('40'),
+                  )),
+              DropdownMenuItem(
+                  value: 50,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('50'),
+                  )),
+            ],
+            onChanged: sortingForm.isDisableActionForm
+                ? null
+                : (int? value) {
+                    final itemValue = value;
+                    if (itemValue != null) sortingForm.setTotalItem(itemValue);
+                  },
+          );
+        },
+      ),
     );
   }
 }
