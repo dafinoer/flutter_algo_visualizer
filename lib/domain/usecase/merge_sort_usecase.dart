8e602300b7ef9fde164faeca015ff28994a74f3b
dafinoer <dafinoer@outlook.com>
2022-11-18 08:29:31 +0700

    bbugfix merge sort

--- lib/domain/usecase/merge_sort_usecase.dart
+++ lib/domain/usecase/merge_sort_usecase.dart
@@ -5,11 +5,10 @@ import 'sorting_usecase.dart';
 
 class MergeSortUseCase implements DivideAndConquer {
   final ReactiveRepository<Visualizer> reactiveRepository;
-  late List<int> _items;
+  List<int>? _items;
+  Map<int, int>? _pointingValue;
 
-  MergeSortUseCase(this.reactiveRepository) {
-    _items = List.empty(growable: true);
-  }
+  MergeSortUseCase(this.reactiveRepository);
 
   @override
   List<int> sorting(List<int> items) {
@@ -26,49 +25,74 @@ class MergeSortUseCase implements DivideAndConquer {
 
   List<int> merge(List<int> left, List<int> right) {
     final items = <int>[];
-    final valueAddList = <int>{};
     int leftIndex = 0;
     int rightIndex = 0;
     while (leftIndex < left.length && rightIndex < right.length) {
-      reactiveRepository.onSetEvent(Visualizer(
-        items: List.of(_items),
-        indexActiveColor: <double>{
-          left[leftIndex].toDouble(),
-          right[rightIndex].toDouble()
-        },
-      ));
+      _onSetEvent({left[leftIndex].toDouble(), right[rightIndex].toDouble()});
       if (left[leftIndex] < right[rightIndex]) {
         items.add(left[leftIndex]);
-        valueAddList.add(left[leftIndex]);
         leftIndex++;
       } else {
+        _onSwitchItem(left[leftIndex], right[rightIndex]);
         items.add(right[rightIndex]);
-        valueAddList.add(right[rightIndex]);
+        _onSetEvent({left[leftIndex].toDouble(), right[rightIndex].toDouble()});
         rightIndex++;
       }
     }
-    if (left.isNotEmpty) {
-      items.addAll(left.where((e) => !valueAddList.contains(e)).toList());
+    if (leftIndex < left.length) {
+      items.addAll(left.getRange(leftIndex, left.length));
+    } else if (rightIndex < right.length) {
+      items.addAll(right.getRange(rightIndex, right.length));
     }
-    if (right.isNotEmpty) {
-      items.addAll(right.where((e) => !valueAddList.contains(e)).toList());
+    final isFullSort = items.length == values.length;
+    final listValues = _items;
+    if (isFullSort && listValues != null) {
+      for (int index = 0; index < items.length; index++) {
+        listValues[index] = items[index];
+        _onSetEvent({items[index].toDouble()});
+      }
+      _onSetEvent(null);
     }
-    final isFullSort = items.length == _items.length;
-    for (int index = 0; index < items.length; index++) {
-      _items[index] = items[index];
-    }
-    reactiveRepository.onSetEvent(Visualizer(
-      items: List.of(_items),
-      indexActiveColor: !isFullSort ? <double>{} : null,
-    ));
     return items;
   }
 
   @override
-  List<int> get values => _items.toList(growable: false);
+  List<int> get values =>
+      _items?.toList(growable: false) ?? List.empty(growable: false);
 
   @override
   void setDefaultItems(List<int> items) {
     _items = items;
+    _createNewPointingMap(items);
+  }
+
+  void _onSetEvent(Set<double>? indexEnableColor) {
+    reactiveRepository.onSetEvent(
+        Visualizer(items: List.of(values), indexActiveColor: indexEnableColor));
+  }
+
+  void _onSwitchItem(int leftValue, int rightValue) {
+    final pointValue = _pointingValue;
+    final items = _items;
+    if (pointValue != null && items != null && items.isNotEmpty) {
+      final resultIndexLeft = pointValue[leftValue];
+      final resultIndexRight = pointValue[rightValue];
+      if (resultIndexLeft != null && resultIndexRight != null) {
+        final tempRight = items[resultIndexRight];
+        items[resultIndexRight] = items[resultIndexLeft];
+        items[resultIndexLeft] = tempRight;
+        pointValue.update(leftValue, (value) => resultIndexRight);
+        pointValue.update(rightValue, (value) => resultIndexLeft);
+      }
+    }
+  }
+
+  void _createNewPointingMap(List<int> items) {
+    int index = 0;
+    _pointingValue = Map<int, int>.of({});
+    for (final item in items) {
+      _pointingValue!.putIfAbsent(item, () => index);
+      index++;
+    }
   }
 }
