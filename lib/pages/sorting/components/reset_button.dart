ebda3a613ea3608bf5b7500ffd8bb23eceb26654
Muhammad Dafi <dafinoer@outlook.com>
2023-03-15 13:56:02 +0700

    on tap reset button

--- lib/pages/sorting/components/reset_button.dart
+++ lib/pages/sorting/components/reset_button.dart
@@ -1,25 +1,28 @@
-import 'package:algovisualizer/presentation/sorting/sorting_form.dart';
-import 'package:algovisualizer/presentation/sorting/sorting_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
+import '../../../presentation/sorting/sorting_form.dart';
+import '../../../presentation/sorting/sorting_store.dart';
+
 class ResetButton extends StatelessWidget {
   const ResetButton({Key? key}) : super(key: key);
 
+  void onResetButtonAction(BuildContext context) {
+    final totalItem = context.read<SortingForm>().totalItem;
+    if (totalItem != null) {
+      context.read<SortingStore>().onResetStore(totalItem);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final sortingStore = context.read<SortingStore>();
-
     return Observer(
       builder: (context) {
         return TextButton(
-          onPressed: sortingStore.isRunning
+          onPressed: context.read<SortingForm>().isDisableActionForm
               ? null
-              : () {
-                  final form = context.read<SortingForm>();
-                  sortingStore.setDataItems(form.totalItem);
-                },
+              : () => onResetButtonAction(context),
           child: const Text('Reset'),
         );
       },
